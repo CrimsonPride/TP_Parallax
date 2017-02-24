990ad3e5b14596fa064b94cf12845fd91a83a86b
Patrick Lauzon <patrick.lauzon94@hotmail.com>
2017-02-24 02:38:48 -0500

    Fini

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 var materia = document.getElementById('btn');
+var meteore = document.getElementById('Meteor');
+
+
 materia.addEventListener("click",function(){
-	console.log("yolo");
+	meteore.style.animation = "apparait 5s linear";
+	materia.style.display = "none";
+	var audio = document.getElementById('audio');
+	audio.play();
 });
+
+meteore.addEventListener("animationend", AnimationListener, false);
+
+function AnimationListener(){
+		meteore.style.opacity = 1;
+}
